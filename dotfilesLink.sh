6704ad62e557e14b90a71e6636c87ddf5a84edca
shirow ozawa <shirow@shirow-no-MacBook-Pro.local>
2019-03-07 11:28:33 +0900

    feat:$B%7%s%\%j%C%/%j%s%/:n@.MQ$N%7%'%k%9%/%j%W%H$rDI2C(B

---
+++ dotfilesLink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitattributes ~/.gitattributes
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
