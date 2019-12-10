ae983147ffb8aa25c6b65ae41cc7cca9fe082e1f
shirow ozawa <striderkein@gmail.com>
2019-12-10 18:12:46 +0900

    feat:gitconfigを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,5 +2,6 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitattributes ~/.gitattributes
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
