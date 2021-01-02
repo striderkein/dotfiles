1cdf28e8dfa79e88fdab57c1407a4af09d6cfc50
shirow ozawa <striderkein@users.noreply.github.com>
2021-01-02 17:55:56 +0900

    style:added comment

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,8 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+# Git settings
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitattributes ~/.gitattributes
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
