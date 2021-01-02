edc14db9f3cf58ce20deeb91cfed7642c94e80c2
shirow ozawa <striderkein@users.noreply.github.com>
2021-01-02 17:59:18 +0900

    refactor:added if clause to detect shell
    macOS Catalina 以降は標準シェルが zsh になったため。

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+# detect bash | zsh
+if [ -n "${BASH_VERSION-}" ]; then
+  ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+  ln -sf ~/dotfiles/.bashrc ~/.bashrc
+elif [ -n "${ZSH_VERSION-}" ]; then
+  ln -sf ~/dotfiles/.bash_profile ~/.zprofile
+  ln -sf ~/dotfiles/.bashrc ~/.zshrc
+# elif [ -n "${RANDOM-}" ]; then
+#   # case: other
+# else
+#   # case: sh
+fi
 
 # Git settings
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
