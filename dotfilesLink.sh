3d88c188bb6b40f3f944846ac3384b2a7897a978
shirow ozawa <striderkein@users.noreply.github.com>
2021-12-09 13:20:01 +0900

    refactor:シンボリックリンクを貼る処理を関数化
    ファイルが増える度にスクリプトの修正が必要だったので。

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,20 +1,41 @@
 #!/bin/sh
 dir=$(cd $(dirname $0) && pwd)
-ln -sf $dir/.vimrc ~/.vimrc
+
+create_homedir_symlink() {
+  local basepath=$dir/$1
+  if [ -z $basepath ]; then
+    return
+  fi
+
+  # 第２引数で出力ファイル名変更可能
+  local outname=$2
+  if [ -z $outname ]; then
+    local outname=$1
+  fi
+
+  local outpath=~/$outname
+  if [ -e $outpath ]; then
+    return
+  fi
+
+  ln -sfnv $basepath $outpath
+}
 
 # detect bash | zsh
 if [ -n "${BASH_VERSION-}" ]; then
-  ln -sf $dir/.profile ~/.profile
-  ln -sf $dir/.bashrc ~/.bashrc
-elif [ -n "${ZSH_VERSION-}" ]; then
-  ln -sf $dir/.profile ~/.zprofile
-  ln -sf $dir/.zshrc ~/.zshrc
-# elif [ -n "${RANDOM-}" ]; then
-#   # case: other
-# else
-#   # case: sh
+  create_homedir_symlink .profile .bash_profile
+  create_homedir_symlink .bashrc
+else
+  create_homedir_symlink .profile .zprofile
+  create_homedir_symlink .zshrc
 fi
 
-# Git settings
-ln -sf $dir/.gitconfig ~/.gitconfig
-ln -sf $dir/.gitignore ~/.gitignore
+for file in .??*
+do
+  [[ "$file" == ".git" ]] && continue
+  [[ "$file" == ".commons" ]] && continue
+  [[ "$file" == ".profile" ]] && continue
+  [[ "$file" == ".bashrc" ]] && continue
+  [[ "$file" == ".zshrc" ]] && continue
+  create_homedir_symlink $file
+done
